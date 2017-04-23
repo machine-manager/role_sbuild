299091f599698148fbedfcba4aa4379892c1c7bb
Ivan Kozik <ivan@ludios.org>
2017-04-23 19:55:39 +0000

    Whitelist the apt-cacher-ng -> custom-packages-server access

--- lib/role.ex
+++ lib/role.ex
@@ -51,10 +51,15 @@ defmodule RoleSbuild do
 			]},
 			ferm_output_chain:
 				"""
-				# apt-cacher-ng
 				outerface lo {
+					# apt -> apt-cacher-ng
 					daddr 127.0.0.1 proto tcp syn dport 3142 {
 						mod owner uid-owner _apt ACCEPT;
+					}
+
+					# apt-cacher-ng -> custom-packages-server
+					daddr 127.0.0.1 proto tcp syn dport 28000 {
+						mod owner uid-owner apt-cacher-ng ACCEPT;
 					}
 				}
 				""",
