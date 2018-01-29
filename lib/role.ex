bac20fa120e930ac7386b9be4fe2a71878701aa9
Ivan Kozik <ivan@ludios.org>
2018-01-29 10:18:46 +0000

    Use valid ferm variable name

--- lib/role.ex
+++ lib/role.ex
@@ -103,7 +103,7 @@ defmodule RoleSbuild do
 			ferm_output_chain:
 				"""
 				# User may not exist yet
-				@def $user_apt-cacher-ng = `(getent passwd apt-cacher-ng > /dev/null && echo apt-cacher-ng) || echo root`;
+				@def $user_apt_cacher_ng = `(getent passwd apt-cacher-ng > /dev/null && echo apt-cacher-ng) || echo root`;
 
 				outerface lo {
 					# {apt, debootstrap} -> apt-cacher-ng
@@ -113,7 +113,7 @@ defmodule RoleSbuild do
 
 					# apt-cacher-ng -> custom-packages-server
 					daddr 127.0.0.1 proto tcp syn dport 28000 {
-						mod owner uid-owner $user_apt-cacher-ng ACCEPT;
+						mod owner uid-owner $user_apt_cacher_ng ACCEPT;
 					}
 
 					# Loopback connections are necessary for running the git test
